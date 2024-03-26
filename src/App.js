d6d2ef6f094809586632cffb400bbec1e565cc4c
bhaveyaryaa <144548924+bhaveyaryaa@users.noreply.github.com>
2024-03-27 03:03:18 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
-
 import Navbaar from "./components/Navbaar";
-
 import Carousel from "./components/Carousel";
 
 function App() {
